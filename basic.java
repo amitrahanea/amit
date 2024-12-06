62b0a8844daf2c8959805e014f05c56439c66fd5
amitrahanea <rahanea29@gmail.com>
2024-12-06 21:16:43 +0530

    Add files via upload
    file added

---
+++ basic.java
@@ -0,0 +1,20 @@
+
+public class basic{
+public static void main(String args[]){
+    //  int num =  20;
+
+    int income = 445;
+    int max ;
+    if(income > 50000){
+      max = (int) (income *0.2);
+      System.out.println(max);
+    } 
+       else if(income>9){
+         System.out.println("djweo");
+
+        }
+
+} 
+}
+    // int max = (income>56)?3:1;
+   
